6ca33dbb12c8cdb7d303f93daabf2e4c41a831a1
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2020-06-10 12:28:07 -0300

    Adicionado suporte ao CORS

--- backend/handler.js
+++ backend/handler.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const CodeService = require('./codeService');
 
 module.exports.handler = async event => {
+  console.log('o tipo do body é: ', typeof event.body);
+  console.log('o body é:', event.body);
   const codeService = new CodeService();
   let res = {};
   try {
@@ -11,12 +13,14 @@ module.exports.handler = async event => {
         statusCode: 200,
         headers: {
           'Content-Type': 'application/zip',
+          'Access-Control-Allow-Origin': '*',
         },
         isBase64Encoded: 'true',
         body: codeService.generatedFile,
       };
     });
   } catch(err) {
+    console.log(err);
     res = {
       statusCode: 500,
       message: 'Internal Server Error',
